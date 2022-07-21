d63c5ecb602a2a03d39c42552e55804a5a7885b0
klekkwedge <antonov.andrey.10101@gmail.com>
2022-07-21 16:36:18 +0300

    feat: add Skeleton component

--- todo/src/components/TaskDetail/TaskDetail.js
+++ todo/src/components/TaskDetail/TaskDetail.js
@@ -1,5 +1,6 @@
-import { Flex, Badge } from "@chakra-ui/react";
+import { Flex, Badge, Stack } from "@chakra-ui/react";
 import { ArrowBackIcon } from "@chakra-ui/icons";
+import { Skeleton, SkeletonCircle, SkeletonText } from "@chakra-ui/react";
 
 import "./TaskDetail.scss";
 
@@ -37,17 +38,20 @@ function TaskDetail({ currentTask }) {
     }
   };
 
-  const detail = () => {
+  const showDetails = () => {
     return (
       <Flex flexDirection="column" gap="2px" position="relative">
         <h3 className="task-detail__name">{currentTask.nameTask}</h3>
-        <h3 className="task-detail__name">
-          {currentTask.complete === "true" ? "Done" : "Active"}
-        </h3>
-        <h3 className="task-detail__name">
+
+        <h3 className="task-detail__category">
           {currentTask.category ? categoryTask() : "Not category"}
         </h3>
-        <h3 className="task-detail__name">
+
+        <h3 className="task-detail__status">
+          {currentTask.complete === "true" ? "Done" : "Active"}
+        </h3>
+
+        <h3 className="task-detail__descr">
           Description:{" "}
           {currentTask.description
             ? currentTask.description
@@ -59,13 +63,21 @@ function TaskDetail({ currentTask }) {
 
   return (
     <section className="task-detail">
-      <h2 className="task-detail__title">Task details </h2>
+      <h2 className="task-detail__title">Task details:</h2>
       {currentTask.nameTask ? (
-        detail()
+        showDetails()
       ) : (
-        <Flex alignItems="center" gap="5px">
-          <ArrowBackIcon />
-          <h3>Please, choose task</h3>
+        <Flex flexDirection="column">
+          <Flex alignItems="center" gap="5px" mb="20px">
+            <ArrowBackIcon />
+            <h3>Please, choose task</h3>
+          </Flex>
+          <Stack>
+            <Skeleton height="20px" />
+            <Skeleton height="20px" />
+            <Skeleton height="20px" />
+            <Skeleton height="20px" />
+          </Stack>
         </Flex>
       )}
     </section>
