ec98417a2ed08c568f90d7ec10630eae5404ef91
Andrey <antonov.andrey.10101@gmail.com>
2023-08-06 15:53:38 +0400

    feat: delete task settings

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,6 +6,7 @@ import BackgroundSelection from '../BackgroundSelection/BackgroundSelection';
 import NewTaskForm from '../NewTaskForm/NewTaskForm';
 import Header from '../Header/Header';
 import './App.scss';
+import Filters from '../Filters/Filters';
 
 function App() {
   return (
